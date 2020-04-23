144c63f859f8497ddbcca0c4232fd4dc7a261227
Bart Heupers <bart.heupers@gmail.com>
2020-04-23 13:20:07 +0200

    Pass all parameters from script to run_import

--- src/start_import.sh
+++ src/start_import.sh
@@ -4,11 +4,10 @@ set -u   # crash on missing env variables
 set -e   # stop on any error
 set -x
 
-CREATE_TABLES=${1:-''}
 DJANGO_SETTINGS_MODULE=dso_import.settings
 DJANGO_DEBUG=false
 
 python -m venv venv
 source venv/bin/activate
 pip install  -r requirements.txt
-python manage.py run_import bagh ${CREATE_TABLES}
+python manage.py run_import bagh "$@"
